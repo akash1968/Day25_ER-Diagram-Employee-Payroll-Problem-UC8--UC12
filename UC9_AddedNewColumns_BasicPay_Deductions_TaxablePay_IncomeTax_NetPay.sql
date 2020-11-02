ce7cbd0e7d5aa400c3f93f1d5922c48702ed034b
Akash <akash.singh670@gmail.com>
2020-11-03 01:24:50 +0530

    [Akash]. Added Payroll_Services DB Assignment Day 25

---
+++ UC9_AddedNewColumns_BasicPay_Deductions_TaxablePay_IncomeTax_NetPay.sql
@@ -0,0 +1,12 @@
+
+--UC 9 : Ability to extend employee_payroll table to have Basic Pay,Deductions,Taxable Pay,Income Tax, Net Pay
+
+use payroll_services;
+select * from employee_payroll;
+--Adding new columns
+alter table employee_payroll add
+BasicPay float,
+Deductions float,
+TaxablePay float,
+Income_Tax float,
+NetPay float;
